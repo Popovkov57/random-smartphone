726cae15538bf90c77aa6de775c1c5feb787e044
Popovkov57 <kevin.patrzek@live.fr>
2022-10-10 13:07:49 +0200

    Init commit

---
+++ src/main/java/com/api/randomsmartphone/RandomSmartphoneApplication.java
@@ -0,0 +1,13 @@
+package com.api.randomsmartphone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomSmartphoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomSmartphoneApplication.class, args);
+	}
+
+}
